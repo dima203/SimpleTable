e827df770cb3eec99e8351076f7243d41cb826e1
dima203 <gg37500@yandex.by>
2025-12-24 11:19:00 +0300

    fix multiline data output; fix min/max table width calculating

--- src/simple_table_dima203/table.py
+++ src/simple_table_dima203/table.py
@@ -1,3 +1,4 @@
+import textwrap
 from typing import Any
 
 
@@ -16,6 +17,7 @@ class Table:
         self.min_table_width = None
 
         self.none_format = ''
+        self.wrap = True
 
         self.vertical_character = "|"
         self.horizontal_character = "="
@@ -93,34 +95,67 @@ class Table:
         columns_length = self.__get_formated_columns_length()
         return (
             "".join(
-                f"{self.vertical_character}{key: ^{columns_length[key]}}"
+                f"{self.vertical_character}{key: {self.align[key]}{columns_length[key]}}"
                 for key in self.headers
             )
             + self.vertical_character
         )
 
     def __get_table_strings(self) -> list[str]:
-        columns_length = self.__get_formated_columns_length()
         strings = []
         for row in self.__data:
-            strings.append(
-                "".join(
-                    f"{self.vertical_character}{str(row[key]) if row[key] is not None else self.none_format: {self.align[key]}{columns_length[key]}}"
-                    for key in row
-                )
-                + self.vertical_character
-            )
+            strings.extend(self.__get_row_strings(row))
         return strings
 
-    def __get_row_strings(self, ) -> list[str]:
-        pass
+    def __get_row_strings(self, row: dict[str, Any]) -> list[str]:
+        columns_length = self.__get_formated_columns_length()
+        row_data_strings = {key: self.__get_data_strings(data, columns_length[key]) for key, data in row.items()}
+        strings = []
+        max_data_length = len(max(row_data_strings.values(), key=len))
+        for i in range(max_data_length):
+            string = ""
+            for key, data in row_data_strings.items():
+                index = len(data) - max_data_length + i
+                if index >= 0:
+                    string += f"{self.vertical_character}{data[index]: {self.align[key]}{columns_length[key]}}"
+                else:
+                    string += f"{self.vertical_character}{"": {self.align[key]}{columns_length[key]}}"
+            string += self.vertical_character
+            strings.append(string)
+        return strings
+
+    def __get_data_strings(self, data: Any, length: int) -> list[str]:
+        return self.__format_data(data, length).split('\n')
+
+    def __format_data(self, data: Any, length: int) -> str:
+        return self.__format_length(self.__format_none(data), length)
+
+    def __format_length(self, data: str, length: int) -> str:
+        return textwrap.fill(
+            data,
+            length,
+            placeholder="",
+            max_lines=None if self.wrap else 1,
+        )
+
+    def __format_none(self, data: Any) -> str:
+        return str(data) if data is not None else self.none_format
 
     def __get_formated_columns_length(self) -> dict[str, int]:
         columns_length = self.__get_max_columns_length()
-        if self.min_table_width is not None:
-            need_length = self.min_table_width - (sum(columns_length.values()) + len(columns_length) + 1)
+        for key, length in columns_length.items():
+            if self.min_width[key] is not None:
+                columns_length[key] = self.min_width[key]
+
+        table_width = sum(columns_length.values()) + len(columns_length) + 1
+
+        if self.min_table_width is not None and table_width < self.min_table_width:
+            need_length = self.min_table_width - table_width
+        elif self.max_table_width is not None and table_width > self.max_table_width:
+            need_length = self.max_table_width - table_width
         else:
             need_length = 0
+
         added_length = self.__get_added_columns_length(need_length, columns_length)
         return {key: columns_length[key] + added_length[key] for key in columns_length}
 
@@ -128,13 +163,20 @@ class Table:
         allocations = {key: 0 for key in current_length}
         weights = current_length.copy()
         total_weight = sum(weights.values())
+        is_growth = True if need_length >= 0 else False  # Нужно увеличить или уменьшить размер таблицы
 
-        for _ in range(need_length):
+        for _ in range(abs(need_length)):
             for key in weights:
-                if self.max_width[key] is not None:
-                    if weights[key] + allocations[key] >= self.max_width[key]:
-                        weights[key] = 0
-                        total_weight = sum(weights.values())
+                if is_growth:
+                    if self.max_width[key] is not None:
+                        if weights[key] + allocations[key] >= self.max_width[key]:
+                            weights[key] = 0
+                            total_weight = sum(weights.values())
+                else:
+                    if self.min_width[key] is not None:
+                        if weights[key] - allocations[key] <= self.min_width[key]:
+                            weights[key] = 0
+                            total_weight = sum(weights.values())
 
             # Для каждого объекта вычисляем, насколько он "недополучил"
             deficits = {}
@@ -146,6 +188,9 @@ class Table:
             # Даем единицу объекту с наибольшим дефицитом
             idx = max(deficits, key=lambda key: deficits[key])
             allocations[idx] += 1
+
+        if not is_growth:
+            allocations = {key: -value for key, value in allocations.items()}
 
         return allocations
 
---
+++ tests/test.py
@@ -0,0 +1,17 @@
+from src.simple_table_dima203 import Table
+
+
+class TestTable:
+    def test_table_print(self) -> None:
+        table = Table(headers=["name", "age"])
+        table.none_format = '-'
+        table.min_table_width = 20
+        table.max_table_width = 30
+        table.max_width["age"] = 3
+        table.min_width["age"] = 3
+        table.align["name"] = "<"
+        table.add_row(["Alex gfjdkljgkdjklfgld", 22])
+        print(table)
+        table.add_column("id", align="<")
+        table.min_width["id"] = 5
+        print(table)
